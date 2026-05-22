f4abd978bd79f0a8ac9c55a083cc6efbeaec629a
IGNIT3-xD <md.imranali089@gmail.com>
2026-05-22 16:42:24 +0600

    Deployment

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@ import express, { type Application, type Request, type Response } from 'express'
 import { globalErrorHandler } from './middleware/globalErrorHandler';
 import { userRoute } from './modules/users/users.route';
 import { issueRoute } from './modules/issues/issues.route';
+import cors from 'cors'
 
 const app: Application = express()
+
+app.use(cors({
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
+
 app.use(express.json())
 
 app.get('/', (req: Request, res: Response) => {
